be5bb56db1ee3ccfefc30f90f071609650c56475
David Bosschaert <bosschae@adobe.com>
2017-08-25 13:43:35 +0100

    Simple interaction works

--- src/main/java/org/coderthoughts/radsiotdemo/RadiatorControlFunction.java
+++ src/main/java/org/coderthoughts/radsiotdemo/RadiatorControlFunction.java
@@ -1,6 +1,9 @@
 package org.coderthoughts.radsiotdemo;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Dictionary;
 
@@ -13,11 +16,14 @@ import org.osgi.service.dal.functions.data.LevelData;
 public class RadiatorControlFunction implements MultiLevelControl {
     private final Dictionary<String, ?> serviceProps;
     private final String uid;
+    private final String deviceIP;
     private volatile LevelData data;
 
     public RadiatorControlFunction(String deviceUID, Dictionary<String, Object> props) {
         uid = deviceUID;
         serviceProps = props;
+
+        deviceIP = deviceUID.equals("radA") ? "172.24.1.22" : "172.24.1.23";
     }
 
     @Override
@@ -51,5 +57,13 @@ public class RadiatorControlFunction implements MultiLevelControl {
     public void setData(BigDecimal level, String unit) throws DeviceException {
         System.out.println("*** Setting radiator " + uid + " to " + level + " " + unit);
         data = new LevelData(System.currentTimeMillis(), null, level, unit);
+
+        String url = "http://" + deviceIP + "/" + (level.intValue() > 15 ? "on" : "off");
+        try {
+            InputStream is = new URL(url).openStream();
+            Streams.suck(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
